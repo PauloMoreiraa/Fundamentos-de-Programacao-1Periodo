024998ca7a598e7413fab66f83eca21a3e17222e
PauloWarren <nagatogts@hotmail.com>
2022-04-10 22:16:20 -0300

    Exercícios

--- lista2 (if else)/js/functions.js
+++ lista2 (if else)/js/functions.js
@@ -49,14 +49,80 @@ function divisivel(){
     }
 }
 
-function maiormenor(){
-    if(n1 == n2){
-        document.write(n1 + ' é igual a ' + n2);
+function menor(){
+    if(n1 < n2){
+        document.write(`${n1} é o menor número informado!`);
     } else if(n1 > n2){
-        document.write(n1 + ' é maior que ' + n2);
+        document.write(`${n2} é o menor número informado!`);
     } else{
-        document.write(n2 + ' é maior que ' + n1);
+        document.write('Os números informados são iguais!')
     }
 }
 
+function maiormenor(){
+    if(n1 == n2){
+        document.write(`${n1} é igual à ${n2}`);;
+    } else if(n1 > n2){
+        document.write(`${n2}, ${n1}`);
+    } else{
+        document.write(`${n1}, ${n2}`);
+    }
+}
+
+function maior(){
+    if (n1 >= n2 && n1 >= n3){
+        document.write(`${n1} é o maior!`);
+    } else if(n2 >= n1 && n2 >= n3){
+        document.write(`${n2} é o maior!`); 
+    } else if(n3 >= n1 && n3 >= n2){
+        document.write(`${n3} é o maior!`); 
+    } else{
+        document.write(`Dados inválidos!`); 
+    }
+}
+
+function diferente(){
+    if(n2 < n1){
+        r = n1 - n2;
+        document.write(`A difenrença entre ${n1} e ${n2} é de ${r}`);
+    } else{
+        alert(`O segundo número PRECISA ser menor do que o primeiro!!!`);
+
+        n1 = Number(prompt('Informe algum número novamente:'));
+        n2 = Number(prompt('Informe outro número novamente:'));
+
+        if(n2 < n1){
+            r = n1 - n2;
+            document.write(`A difenrença entre ${n1} e ${n2} é de ${r}`);
+        } else{
+            alert(`O segundo número PRECISA ser menor do que o primeiro!!!!!!!!!!!!!!!!!!!!!`);
+        }
+    }
+}
+
+function maiorDiferente(){
+    if(n1 == n2){
+        alert('Os números PRECISAM ser diferentes!');
+
+        n1 = Number(prompt('Informe algum número novamente:'));
+        n2 = Number(prompt('Informe outro número novamente:'));
+
+        if(n1 == n2){
+            alert('OS NÚMEROS PRECISAM SER DIFERENTES!!!!')
+        }else if(n1 > n2){
+            document.write(`${n1} é o maior`);
+        } else{
+            document.write(`${n2} é o maior`);
+        }
+
+    }else if(n1 > n2){
+        document.write(`${n1} é o maior`);
+    } else{
+        document.write(`${n2} é o maior`);
+    }
+}
+
+function dosa(){
+    
+}
   
